ea97e00f73b83e148ba14f96d2c82a3bae68401a
Gabriele De Carlo <gibiai123@gmail.com>
2023-06-18 19:08:50 +0200

    Add files via upload

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gde-carl <gde-carl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 01:10:29 by gibiai            #+#    #+#             */
-/*   Updated: 2023/04/07 19:40:04 by gde-carl         ###   ########.fr       */
+/*   Updated: 2023/06/18 19:05:10 by gde-carl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	if ((!dest && !src) || (src == dest))
 		return (NULL);
 	if ((size_t)dest - (size_t)src < n)
-	{
+	{	
 		i = n - 1;
-		while (i >= 0 && i < n)
+		while (i > 0 && i < n)
 		{
 			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-			i--;
+			i --;
 		}
 	}
 	else
 	{
 		while (i < n)
-		{
+		{	
 			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-			i++;
+			i ++;
 		}
 	}
 	return (dest);
